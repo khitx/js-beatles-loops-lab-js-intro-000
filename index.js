f9fad5631c9937de6a0db44ce3675f2c0d3e2350
Kathy Hudson <khitx@icloud.com>
2017-11-21 17:26:12 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,7 @@
+function theBeatlesPlay(musicians, instruments) {
+  var array = new Array()
+  for (var i = 0, i < musicians.length, i++) {
+    array = musicians[i] . instruments[i]
+  }
+  return array
+}
