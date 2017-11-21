894769c0b1b69f9a4100832525cf928c103e3985
Kathy Hudson <khitx@icloud.com>
2017-11-21 17:32:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians, instruments) {
   var array = new Array()
-  for (var i = 0, i < musicians.length, i++) {
+  for (var i = 0; i < musicians.length; i++) {
     array = musicians[i] . instruments[i]
   }
   return array
